44f34b87e98400651c36b14d96b70a9d4432d908
DamaZeSzczurem <czarna.kawe.prosze@gmail.com>
2021-01-29 00:25:27 +0100

    added answear to task 2

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,25 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt("Dla jakiej liczby podać Ci tabliczkę mnożenia?");
 
-
-
+if(x<=9){
+  for (let i = 1; i < 9; i++) {
+    console.log(x + " * " + i + " = " + x*i);
+  }
+} else{
+    console.log("Hej! To nie jest odpowiednia liczba!");
+}
 /* rozwiązanie z pętlą while  */
+const y = prompt("jaka jest podstawa potęgowania?");
+const k =  prompt("jaki jest wykładnik potęgowania?");
+const text = " * " + y;
+let score = y;
+let n=1;
+
+while (n<=k) {
+  if (n=k){
+    console.log( y + text.repeat(k-1) + " = " + score);
+  }
+  score = score * y;
+  n++;
+}
